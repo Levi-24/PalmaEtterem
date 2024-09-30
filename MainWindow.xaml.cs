4b12fb9a2ec61e88b0efedcf1ecec988f051bd27
Levi-24 <leventemajor36@gmail.com>
2024-09-30 11:21:14 +0200

    Add project files.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System.IO;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace PalmaEtterem
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            List<Sutemeny> sutemenyek = new List<Sutemeny>();
+
+            InitializeComponent();
+            using StreamReader streamReader = new StreamReader(@"../../../src/palma.txt", Encoding.UTF8);
+            while (!streamReader.EndOfStream) sutemenyek.Add(new Sutemeny(streamReader.ReadLine()));
+
+            Random rnd = new Random();
+            LblAjanlat.Content = sutemenyek[rnd.Next(0, sutemenyek.Count)].Nev;
+
+            LblBestCake.Content = sutemenyek.Count(x => x.Dijazott) + " féle díjnyertes édességből választhat!";
+
+            Sutemeny Legdragabb = sutemenyek.Select(s => s).OrderByDescending(s => s.Ar).First();
+            LblDragaNev.Content = Legdragabb.Nev;
+            LblDragaAr.Content = Legdragabb.Ar + "/" + Legdragabb.Egyseg;
+
+            Sutemeny Legolcsobb = sutemenyek.Select(s => s).OrderByDescending(s => s.Ar).Last();
+            LblOlcsoNev.Content = Legolcsobb.Nev;
+            LblOlcsoAr.Content = Legolcsobb.Ar + "/" + Legolcsobb.Egyseg;
+        }
+    }
+}
---
+++ Sutemeny.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PalmaEtterem
+{
+    class Sutemeny
+    {
+        public string Nev { get; set; }
+        public string Tipus { get; set; }
+        public bool Dijazott { get; set; }
+        public int Ar { get; set; }
+        public string Egyseg { get; set; }
+
+        public Sutemeny(string s)
+        {
+            var darabok = s.Split(';');
+            Nev = darabok[0];
+            Tipus = darabok[1];
+            Dijazott = bool.Parse(darabok[2]);
+            Ar = int.Parse(darabok[3]);
+            Egyseg = darabok[4];
+        }
+    }
+}
